8efead8103b9053dc86a2b506d34eba2cb5d0e4e
Victor Miguel Armenta Carrillo <victorarmenta30@gmail.com>
2019-06-29 19:52:09 -0500

    first commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+class Formulario extends Component {
+  state = {
+    categoria: 'general',
+  };
+
+  cambiarCategoria = e => {
+    this.setState({
+      categoria: e.target.value,
+    }, () => {
+        this.props.consultarNoticias(this.state.categoria);
+    });
+    
+    //pasar a la pagina principal
+  };
+  render() {
+    return (
+      <div className="buscador row">
+        <div className="col s12 m8 offset-m2">
+          <form>
+            <h2>Encuentra Noticias por Categoria</h2>
+            <div className="input-field col s12">
+              <select onChange={this.cambiarCategoria}>
+                <option value="general">General</option>
+                <option value="business">Negocios</option>
+                <option value="entertainment">Entrenimiento</option>
+                <option value="health">Salud</option>
+                <option value="science">Ciencia</option>
+                <option value="sports">Deportes</option>
+                <option value="technology">Tecnología</option>
+              </select>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+Formulario.propTypes = {
+  consultarNoticias: PropTypes.func.isRequired,
+};
+
+export default Formulario;
